e3ef71ff831f4fa984fd3ff58ab1ffaecd3674fa
Sri-raman-prasad <94214878+Sri-raman-prasad@users.noreply.github.com>
2024-09-01 12:47:05 +0530

    Create 05_Chapter1_PS.js

---
+++ 05_Chapter1_PS.js
@@ -0,0 +1,33 @@
+// Chapter 1 - Q1
+let a = "Harry"
+let b = 6
+console.log(a + b)// concatecate : Harry6
+
+// Chapter 1 - Q2
+console.log(typeof (a+b))//string
+//string
+
+// Chapter 1 - Q3
+const a1 = {
+  name: "Harry",
+  section: 1,
+  isPrincipal: false
+}
+// a1 = 45
+// a1 = {}
+
+// Chapter 1 - Q4
+a1['friend'] = "Shubham"  
+a1['name'] = "Lovish"  
+console.log(a1)  
+//adds both value pair in existing object
+
+// Chapter 1 - Q5
+const dict = {
+  appreciate: "recognize the full worth of.",
+  ataraxia: "a state of freedom from emotional disturbance and anxiety",
+  yakka: "work, especially hard work."
+}
+
+console.log(dict['yakka']) //shows meaning
+console.log(dict.yakka) // shows meaning
